3c324d0467cb4ede3a75166f99a3a0292bfd8fe3
cchoi <caleb.choi@uwaterloo.ca>
2020-01-18 17:47:40 -0500

    Implemented seed node env variable injection

--- server/src/main/scala/membership/addresser/AddressRetriever.scala
+++ server/src/main/scala/membership/addresser/AddressRetriever.scala
@@ -5,12 +5,21 @@ import schema.ImplicitDataConversions._
 
 trait AddressRetriever {
 
-  // Get the IP address of this process so other nodes can reach
+  /**
+   * The IP address of this node, where other nodes can use this IP address to  contact
+   * this node
+   *
+   * @return the IP address of this node
+   */
   def selfIP: IpAddress =
     sys.env("SELF_IP")
 
-  // Get the IP address of the seed node for first contact when first
-  // joining the cluster
+  /**
+   * The IP address of the seed node for first contact when first joining the cluster
+   *
+   * @return an optional IpAddress, will be None if the variable was not specified and
+   *         implies that this node is the very first node in the cluster
+   */
   def seedIP: Option[IpAddress] =
     sys.env.get("SEED_IP").map(IpAddress(_))
 }
--- server/src/main/scala/membership/addresser/KubernetesAddresser.scala
+++ server/src/main/scala/membership/addresser/KubernetesAddresser.scala
@@ -1,7 +1,13 @@
 package membership.addresser
 
+import java.net.InetAddress
+
 import com.risksense.ipaddr.IpAddress
+import membership.MembershipActor
+import org.slf4j.LoggerFactory
 import schema.ImplicitDataConversions._
+
+import scala.util.{Failure, Success, Try}
 
 
 object KubernetesAddresser extends AddressRetriever {
@@ -10,4 +16,27 @@ object KubernetesAddresser extends AddressRetriever {
   // https://stackoverflow.com/questions/30746888/how-to-know-a-pods-own-ip-address-from-inside-a-container-in-the-pod
   override def selfIP: IpAddress =
     sys.env("MY_POD_IP")
+
+  override def seedIP: Option[IpAddress] = {
+
+    val log = LoggerFactory.getLogger(MembershipActor.getClass)
+
+    sys.env.get("SEED_NODE") match {
+
+      case Some(seedHostname) =>
+        log.info(s"Retrieved seed node environment variable with value: '${seedHostname}'")
+
+        Try(IpAddress(InetAddress.getByName(seedHostname).getHostAddress)) match {
+          case Success(ipAddress) => Some(ipAddress)
+          case Failure(e) =>
+            log.error(s"Could not resolve hostname: ${e}")
+            None
+        }
+
+      case None =>
+        log.info("Seed node environment variable was not found")
+        None
+    }
+  }
+
 }
